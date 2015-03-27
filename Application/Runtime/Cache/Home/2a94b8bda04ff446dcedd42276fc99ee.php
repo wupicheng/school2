9da7f820aa0745febb85390b19b1e468adce5b65
wupicheng <wupicheng@qq.com>
2015-03-27 08:23:33 +0800

    1
    1

---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,21 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>校园网首页</title>
+    <link type="text/css" href="Public/css/index.css" rel="stylesheet">
+</head>
+<body>
+    <div id="top">top</div>
+    <div id="banner">banner</div>
+    <div id="navi">navi</div>
+    <div id="content">
+        <div id="content_left">content_left</div>
+        <div id="content_center">content_center</div>
+        <div id="content_right">content_right</div>
+
+    </div>
+    <div id="footer">footer</div>
+
+</body>
+</html>
